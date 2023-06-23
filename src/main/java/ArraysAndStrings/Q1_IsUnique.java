0a7dd1390b5d78123a097a8fe30e9b0c16056200
hatsh3p <sarahamarragy@gmail.com>
2023-06-23 14:55:24 -0700

    Completed first question and tests.

--- src/main/java/ArraysAndStrings/Q1_IsUnique.java
+++ src/main/java/ArraysAndStrings/Q1_IsUnique.java
@@ -1,7 +1,79 @@
-package 1.Arrays&Strings;
+package ArraysAndStrings;
 
-public class IsUnique {
-    public static void main(String[] args) {
+public class Q1_IsUnique {
+    /**
+     * Problem: Implement an algorithm to determine if a string has all unique characters.
+     *      Input:       Output:
+     *      "cat"        "true"
+     *      "bib"        "false"
+     */
 
+    /**
+     * Solution 1
+     *
+     *      Time Complexity:     O(N)
+     *      Space Complexity:    O(N)
+     */
+    public static boolean isUnique(String str) {
+        int[] codepoints = new int[128]; // Represents the ASCII character set.
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i); // 'A' = 41.
+            codepoints[c]++;
+        }
+        for (int i = 0; i < codepoints.length; i++) {
+            if (codepoints[i] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Solution 2: Based on CTCI Solution 1.
+     *
+     *      Time Complexity:     O(N)
+     *      Space Complexity:    O(N)
+     */
+    public static boolean isUniqueModified(String str) {
+        if (str.length() > 128) { // Early exit.
+            return false;
+        }
+
+        boolean[] codepoints = new boolean[128]; // Represents the ASCII character set.
+
+        // Use boolean instead in int.
+        // Check the current value to allow for early exit instead of looping again.
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i); // 'A' = 41.
+            if (codepoints[c] == true) {
+                return false;
+            } else {
+                codepoints[c] = true;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Solution 3: No additional data structures.
+     *
+     *      Time Complexity:     O(N**2)
+     *      Space Complexity:    O(1)
+     */
+    public static boolean isUniqueNoAdditionalDS(String str) {
+        if (str.length() > 128) { // Early exit.
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
--- src/test/java/ArraysAndStrings/Q1_IsUniqueTest.java
+++ src/test/java/ArraysAndStrings/Q1_IsUniqueTest.java
@@ -1,4 +1,39 @@
+package ArraysAndStrings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static ArraysAndStrings.Q1_IsUnique.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class Q1_IsUniqueTest {
-  
+    @Test
+    void testIsUniqueFalse() {
+        assertFalse(isUnique("bilbo"));
+    }
+
+    @Test
+    void testIsUniqueTrue() {
+        assertTrue(isUnique("dog"));
+    }
+
+    @Test
+    void testIsUniqueModifiedFalse() {
+        assertFalse(isUniqueModified("bilbo"));
+    }
+
+    @Test
+    void testIsUniqueModifiedTrue() {
+        assertTrue(isUniqueModified("dog"));
+    }
+
+    @Test
+    void testIsUniqueNoAdditionalDSFalse() {
+        assertFalse(isUniqueNoAdditionalDS("bilbo"));
+    }
+
+    @Test
+    void testIsUniqueNoAdditionalDSTrue() {
+        assertTrue(isUniqueNoAdditionalDS("dog"));
+    }
 }
